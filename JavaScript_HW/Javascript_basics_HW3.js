0a86855c78d070f0898065ec9fc55b805f178ab7
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2021-07-26 16:59:59 -0600

    ..

--- JavaScript_HW/Javascript_basics_HW3.js
+++ JavaScript_HW/Javascript_basics_HW3.js
@@ -1,15 +1,16 @@
 //Exercise 1
 //Create a function that adds up the integers in the following array.
 
-//var numbers = [10, 20, 30, 40, 50];
-//
-//function addNumbers(numberArray) {
-//    var result = 0;
-//    for(var i = 0; i < numbers.length; i++) {
-//        result = result + numberArray(i)
-//    }
-//}
+var numbers = [10, 20, 30, 40, 50];
 
+function addNumbers(numberArray) {
+    var result = 0;
+        for(var i = 0; i < numbers.length; i++) {
+        result = result + numberArray(i)
+    }
+        return result;
+}
+        console.log(result);
 
 //function addNumbers(numberArray) {
 
