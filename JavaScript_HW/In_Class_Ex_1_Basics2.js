5082664cc4b5364531f98be0a95bc09d2246248a
bjoobeen <84108654+bjoobeen@users.noreply.github.com>
2021-07-26 16:25:12 -0600

    completed exercises

--- JavaScript_HW/In_Class_Ex_1_Basics2.js
+++ JavaScript_HW/In_Class_Ex_1_Basics2.js
@@ -11,9 +11,28 @@ var hoursofSleep = 8
 
     if (hoursofSleep <= 6) {
         console.log("I am Groggy");    
-}   else (hoursofSleep > 6) {
+}   else (hoursofSleep > 6); {
         console.log("I feel fantastic?");
 } 
 
 //Ex 2
 
+var nextLocation = "home"
+
+console.log("It's been a long day, let's go " + nextLocation);
+
+//Ex 3
+
+//Write an else if statement that if the nextLocation is equal to work, write to the screen
+//“Good morning, finding the fastest route to work!”
+ 
+//?Ex 5 Change the nextLocation to different inputs to change the if statement
+
+var nextLocation = "work";
+
+if (nextLocation = "work") {
+        console.log("Good morning, finding the fastest route to " + nextLocation);
+//Ex 4
+} else {                                                                
+       console.log("Good morning, finding the fastest route to " + nextLocation);
+}
